5db2f3a28708637cc9408cc6529ee2c71f3e593a
Theo Stoffelbach <theo.stoffelbach@ynov.com>
2025-12-24 11:03:49 +0100

    test CICD

--- app/page.tsx
+++ app/page.tsx
@@ -168,7 +168,7 @@ export default function Home() {
           </div>
         </div>
       </section>
-
+      {/* Test */}
       {/* MUSIQUE Section - 3 morceaux */}
       <section id="musique" className="py-16 sm:py-20 px-4 sm:px-6 lg:px-8">
         <div className="max-w-6xl mx-auto">
